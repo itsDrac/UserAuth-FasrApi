5ca4e89fb9fa21c7b65b8697ca3ca9b4525629f0
Sahaj <gpt.sahaj28@gmail.com>
2021-04-21 15:49:05 +0530

    Starter template

---
+++ api/__init__.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+app = FastAPI(
+	title='Fast-api User Auth',
+	description= 'Learing how to `OAuth2` in FastAPI',
+	version = '1.1.1'
+)
+
+register_tortoise(
+	app,
+	db_url='sqlite://:memory:',
+	modules={"models": ["api.models"]},
+	generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+from api import routes 
